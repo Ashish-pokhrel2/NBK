a4de79b5c871462e05ccbe9d10b7872b92636832
Ashish-pokhrel2 <aashish.pokhrel.646@gmail.com>
2025-07-06 20:39:38 +0545

    mergedFrontend

---
+++ MergedFrontend/src/admin/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const isAdminLoggedIn = localStorage.getItem('isAdminLoggedIn') === 'true';
+  if (!isAdminLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+export default ProtectedRoute; 
